59181eff3a01a03403e67d2552851d6d65863a93
sun_cui_hua <sun_cui_hua@163.com>
2018-03-23 16:16:45 +0800

    Initial Commit

---
+++ SCHPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SCHPlayer
+//
+//  Created by Keyto on 2018/3/23.
+//  Copyright © 2018年 CGZD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SCHPlayer/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SCHPlayer
+//
+//  Created by Keyto on 2018/3/23.
+//  Copyright © 2018年 CGZD. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
